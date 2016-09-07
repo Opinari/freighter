a96cecddeb1d8e22cfc3365424848f298c21e956
Dave <daves125125@gmail.com>
2016-09-07 19:46:22 +0100

    freighter cleanup

--- client/client.go
+++ client/client.go
@@ -26,33 +26,39 @@ func RestoreFile(restoreFilePath string, remoteFilePath string) {
 	}
 
 	// Unarchive Files
-	_, err = archive.UnarchiveFile(compressedFile, restoreFilePath)
+	_, err = archive.Unarchive(compressedFile, restoreFilePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Cleanup tmp Compressed and Archived files
+	// Cleanup tmp Downloaded and Uncompressed files
 	os.Remove(downloadedFilePath)
 	os.Remove(compressedFile)
 }
 
-// TODO WIP
-func BackupFile(archiveDir string, backupFilePath string, remoteFilePath string) {
+func BackupDirectory(backupFilePath string, remoteFilePath string) {
 
-	// Archive File
-	archivedFile := archive.ArchiveFile(archiveDir, backupFilePath + ".tar")
-	log.Printf("Files were archived to: '%s'", archivedFile)
+	// Archive Files
+	archivedFilePath, err := archive.Archive(backupFilePath, backupFilePath + ".tar")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Compress File
-	compressedFile := compress.CompressFile(archivedFile, archivedFile + ".gz")
-	log.Printf("File was compressed to: '%s'", compressedFile)
+	compressedFilePath, err := compress.CompressFile(archivedFilePath, archivedFilePath + ".gz")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Upload File
-	uploadedFile, err := dropbox.UploadFile(backupFilePath, remoteFilePath)
+	_, err = dropbox.UploadFile(compressedFilePath, remoteFilePath)
 	if err != nil {
-		panic("Done For!")
+		log.Fatal(err)
 	}
-	log.Printf("File was uploaded to: '%s'", uploadedFile)
+
+	// Cleanup tmp Archived and Compressed files
+	os.Remove(archivedFilePath)
+	os.Remove(compressedFilePath)
 }
 
 func AgeRemoteFile(outputDir string, remoteFilePath string) {
--- compress/compress.go
+++ compress/compress.go
@@ -9,27 +9,71 @@ import (
 	"io"
 )
 
-// TODO Fix this
 const tmpUncompressedFileName = "/tmp.tar"
 
-// TODO NO-OP
-func CompressFile(inputDirPath string, outputFilePath string) (archivedFilePath string) {
+func CompressFile(uncompressedFilePath string, compressedFilePath string) (outputFilePath string, err error) {
 
-	log.Printf("Compressing file from: %s to: %s", inputDirPath, outputFilePath)
+	log.Printf("Compressing file from: %s to: %s", uncompressedFilePath, compressedFilePath)
 
-	archivedFilePath = outputFilePath
+
+	// Create compressed file
+	compressedFile, err := os.Create(compressedFilePath)
+	if err != nil {
+		return "", fmt.Errorf("Error occured opening compressed file: %s", err.Error())
+	}
+	defer compressedFile.Close()
+
+
+	// Open GzipWriter
+	gzipWriter := gzip.NewWriter(compressedFile)
+	defer gzipWriter.Close()
+
+
+	// Open uncompressed file
+	uncompressedFile, err := os.Open(uncompressedFilePath)
+	if err != nil {
+		return "", fmt.Errorf("Error occured finding uncompressed file: %s", err.Error())
+	}
+	defer uncompressedFile.Close()
+
+	reader := bufio.NewReader(uncompressedFile)
+	buffer := make([]byte, 4096)
+	for {
+		n, err := reader.Read(buffer)
+		if err != nil && err != io.EOF {
+			return "", fmt.Errorf("Error occured during reading uncompressed file: %s", err.Error())
+		}
+
+		if n == 0 {
+			break
+		}
+
+		_, err = gzipWriter.Write(buffer[:n])
+		if err != nil {
+			return "", fmt.Errorf("Error writing compressed file: %s", err.Error())
+		}
+	}
+
+	err = gzipWriter.Flush()
+	if err != nil {
+		return "", fmt.Errorf("Error flushing to compressed file: %s", err.Error())
+	}
+
+	log.Printf("File was compressed to: '%s'", compressedFilePath)
+
+	outputFilePath = compressedFilePath
 	return
 }
 
-func UncompressFile(compressedFilePath string, outputDirPath string) (uncompressDirPath string, err error) {
+func UncompressFile(compressedFilePath string, uncompressedFileDir string) (outputFilePath string, err error) {
 
-	log.Printf("Uncompressing file from: %s to: %s", compressedFilePath, outputDirPath)
+	log.Printf("Uncompressing file from: %s to: %s", compressedFilePath, uncompressedFileDir)
+
 
 	// Open compressed file
 	compressedFile, err := os.Open(compressedFilePath)
 	if err != nil {
 		return "", fmt.Errorf("Error occured finding compressed file: %s", err.Error())
-
 	}
 	defer compressedFile.Close()
 
@@ -44,7 +88,7 @@ func UncompressFile(compressedFilePath string, outputDirPath string) (uncompress
 
 
 	// Open uncompressed file to write to
-	uncompressedFilePath := outputDirPath + tmpUncompressedFileName
+	uncompressedFilePath := uncompressedFileDir + tmpUncompressedFileName
 	uncompressedFile, err := os.Create(uncompressedFilePath)
 	if err != nil {
 		return "", fmt.Errorf("Error creating uncompressed file: %s", err.Error())
@@ -75,14 +119,8 @@ func UncompressFile(compressedFilePath string, outputDirPath string) (uncompress
 
 	}
 
-	// TODO Doing a double close here
-	err = uncompressedFile.Close()
-	if err != nil {
-		return "", fmt.Errorf("Error closing uncompressed file: %s", err.Error())
-	}
-
 	log.Printf("Uncompressed file successfully to: %s", uncompressedFilePath)
 
-	uncompressDirPath = uncompressedFilePath
+	outputFilePath = uncompressedFilePath
 	return
 }
